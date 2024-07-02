1ea06c5af2a39270889fd624745dd4a738ff5989
Đá Biển <piaggo120347@gmail.com>
2024-07-02 20:51:42 +0700

    first

---
+++ WebOrderDoAn/src/main/java/JAVAC5/com/WebOrderDoAn/WebOrderDoAnApplication.java
@@ -0,0 +1,13 @@
+package JAVAC5.com.WebOrderDoAn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebOrderDoAnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebOrderDoAnApplication.class, args);
+	}
+
+}
---
+++ WebOrderDoAn/src/test/java/JAVAC5/com/WebOrderDoAn/WebOrderDoAnApplicationTests.java
@@ -0,0 +1,13 @@
+package JAVAC5.com.WebOrderDoAn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebOrderDoAnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
