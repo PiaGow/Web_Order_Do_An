0f95061d65448d722ef91c8ad2225172325d65ee
Đá Biển <piaggo120347@gmail.com>
2024-07-06 02:18:42 +0700

    css+js+templats home,food menu

--- WebOrderDoAn/src/main/java/JAVAC5/com/WebOrderDoAn/Controllers/HomeController.java
+++ WebOrderDoAn/src/main/java/JAVAC5/com/WebOrderDoAn/Controllers/HomeController.java
@@ -1,17 +1,22 @@
 package JAVAC5.com.WebOrderDoAn.Controllers;
 
+import JAVAC5.com.WebOrderDoAn.Services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-
+import JAVAC5.com.WebOrderDoAn.Entities.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
 
 @Controller
 @RequestMapping("/Home")
 public class HomeController {
+    @Autowired
+    private CategoryService categoryService;
     @GetMapping
     public String home(@AuthenticationPrincipal User user, @AuthenticationPrincipal OAuth2User oidcUserService, Model model) {
         if(user != null) {
@@ -20,6 +25,7 @@ public class HomeController {
         else if(oidcUserService != null) {
             model.addAttribute("username", oidcUserService.getName());
         }
+        model.addAttribute("categories", categoryService.getAllCategories());
         return "Home/index";
     }
 }
